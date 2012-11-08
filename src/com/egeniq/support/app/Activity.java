d0c29da3c63080e678896de84874093ad559bf60
Peter Verhage <peter@egeniq.com>
2012-11-08 16:45:13 +0100

    * Added support for support library and non-support library activity / fragments.

---
+++ src/com/egeniq/support/app/Activity.java
@@ -0,0 +1,10 @@
+package com.egeniq.support.app;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Base activity class when using the support library.
+ */
+public class Activity extends FragmentActivity {
+
+}
