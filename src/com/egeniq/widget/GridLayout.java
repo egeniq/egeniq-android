8c977336afe031dfadbf268e3e9c08631200be85
Özcan Kaymak <ozcan@egeniq.com>
2013-05-15 13:43:36 +0200

    GridLayout: add support for extraRows

--- src/com/egeniq/widget/GridLayout.java
+++ src/com/egeniq/widget/GridLayout.java
@@ -12,12 +12,13 @@ import android.view.ViewGroup;
  */
 public class GridLayout extends ViewGroup {
     private int _rowCount = 1;
+    private int _extraRowCount = 0;
     private int _columnCount = 1;
-    private int _extraColumnCount = 0;    
-    
+    private int _extraColumnCount = 0;
+
     private int _rowPadding = 0;
     private int _columnPadding = 0;
-    
+
     /**
      * Layout parameters for component.
      */
@@ -30,12 +31,12 @@ public class GridLayout extends ViewGroup {
         public int topAdjustment;
         public int widthAdjustment;
         public int heightAdjustment;
-        
+
         private int left;
         private int top;
         private int right;
         private int bottom;
-        
+
         /**
          * Constructor.
          * 
@@ -44,7 +45,7 @@ public class GridLayout extends ViewGroup {
          */
         public LayoutParams(int row, int column) {
             this(row, 1, column, 1);
-        }        
+        }
 
         /**
          * Constructor.
@@ -61,13 +62,13 @@ public class GridLayout extends ViewGroup {
             this.column = column;
             this.columnSpan = columnSpan;
         }
-        
+
         /**
          * Adjusts the coordinates and/or width/height of the view with the given amount of display units.
          * 
          * @param left   Shifts the left offset of the view with the given amount of display units (a negative value is possible).
          * @param top    Shifts the top offset of the view with the given amount of display units (a negative value is possible).
-         * @param width  Makes the view the given amount of display units wider (or smaller in case of a negative value).   
+         * @param width  Makes the view the given amount of display units wider (or smaller in case of a negative value).
          * @param height Makes the view the given amount of display units taller (or shorter in case of a negative value).
          */
         public void setAdjustments(int left, int top, int width, int height) {
@@ -77,7 +78,7 @@ public class GridLayout extends ViewGroup {
             heightAdjustment = height;
         }
     }
-    
+
     /**
      * Constructor.
      * 
@@ -86,7 +87,7 @@ public class GridLayout extends ViewGroup {
     public GridLayout(Context context) {
         super(context);
     }
-    
+
     /**
      * Constructor.
      * 
@@ -96,7 +97,7 @@ public class GridLayout extends ViewGroup {
     public GridLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
-    
+
     /**
      * Constructor.
      * 
@@ -106,8 +107,8 @@ public class GridLayout extends ViewGroup {
      */
     public GridLayout(Context context, AttributeSet attrs, int arg) {
         super(context, attrs, arg);
-    }    
-     
+    }
+
     /**
      * Returns the row count.
      * 
@@ -116,7 +117,7 @@ public class GridLayout extends ViewGroup {
     public int getRowCount() {
         return _rowCount;
     }
-    
+
     /**
      * Sets the row count.
      * 
@@ -126,7 +127,26 @@ public class GridLayout extends ViewGroup {
         _rowCount = rowCount;
         requestLayout();
     }
- 
+
+    /**
+     * Returns the extra row count (the number of rows in the row count that are extra).
+     * 
+     * @return extra row count
+     */
+    public int getExtraRowCount() {
+        return _extraRowCount;
+    }
+
+    /**
+     * Sets the extra row count.
+     * 
+     * @param extraRowCount
+     */
+    public void setExtraRowCount(int extraRowCount) {
+        _extraRowCount = extraRowCount;
+        requestLayout();
+    }
+
     /**
      * Returns the column count.
      * 
@@ -135,7 +155,7 @@ public class GridLayout extends ViewGroup {
     public int getColumnCount() {
         return _columnCount;
     }
-    
+
     /**
      * Sets the column count (this includes any extra columns).
      * 
@@ -143,19 +163,19 @@ public class GridLayout extends ViewGroup {
      */
     public void setColumnCount(int columnCount) {
         _columnCount = columnCount;
-        requestLayout();        
+        requestLayout();
     }
-    
+
     /**
-     * Returns the extra column count (the number of columns
+     * Returns the extra column count (the number of columns 
      * in the column count that are extra).
      * 
      * @return extra column count
      */
     public int getExtraColumnCount() {
         return _extraColumnCount;
-    }    
-        
+    }
+
     /**
      * Sets the extra column count.
      * 
@@ -163,9 +183,9 @@ public class GridLayout extends ViewGroup {
      */
     public void setExtraColumnCount(int extraColumnCount) {
         _extraColumnCount = extraColumnCount;
-        requestLayout();        
-    }   
-    
+        requestLayout();
+    }
+
     /**
      * Returns the row padding.
      * 
@@ -174,7 +194,7 @@ public class GridLayout extends ViewGroup {
     public int getRowPadding() {
         return _rowPadding;
     }
-    
+
     /**
      * Sets the row padding.
      * 
@@ -183,7 +203,7 @@ public class GridLayout extends ViewGroup {
     public void setRowPadding(int padding) {
         _rowPadding = padding;
     }
-    
+
     /**
      * Returns the column padding.
      * 
@@ -192,7 +212,7 @@ public class GridLayout extends ViewGroup {
     public int getColumnPadding() {
         return _columnPadding;
     }
-    
+
     /**
      * Sets the column padding.
      * 
@@ -207,25 +227,25 @@ public class GridLayout extends ViewGroup {
      */
     public void setMinimumWidth(int width) {
         super.setMinimumWidth(width);
-        
+
         // workaround for the measured width not being recalculated
         if (getMeasuredWidth() < width && getMeasuredHeight() != 0) {
             setMeasuredDimension(width, getMeasuredHeight());
         }
     }
-    
+
     /**
      * Set minimum height.
      */
     public void setMinimumHeight(int height) {
         super.setMinimumHeight(height);
-        
-        // workaround for the measured height not being recalculated        
+
+        // workaround for the measured height not being recalculated
         if (getMeasuredHeight() < height && getMeasuredWidth() != 0) {
             setMeasuredDimension(getMeasuredWidth(), height);
         }
     }
-    
+
     /**
      * Measure components.
      * 
@@ -236,42 +256,46 @@ public class GridLayout extends ViewGroup {
             super.onMeasure(widthMeasureSpec, heightMeasureSpec);
             return;
         }
-        
+
         int paddingLeft = getPaddingLeft();
         int paddingTop = getPaddingTop();
         int paddingRight = getPaddingRight();
         int paddingBottom = getPaddingBottom();
-        
+
         int width = MeasureSpec.getSize(widthMeasureSpec) - paddingLeft - paddingRight;
         int height = MeasureSpec.getSize(heightMeasureSpec) - paddingTop - paddingBottom;
-        
+
         // make the grid wider than its parent so the extra columns are initially off screen
-        double factor = (double)getColumnCount() / ((double)getColumnCount() - (double)getExtraColumnCount());
-        width = (int)Math.floor(width * factor);
-        
+        double widthFactor = (double)getColumnCount() / ((double)getColumnCount() - (double)getExtraColumnCount());
+        width = (int)Math.floor(width * widthFactor);
+
+        // make the grid taller than its parent so the extra rows are initially off screen
+        double heightFactor = (double)getRowCount() / ((double)getRowCount() - (double)getExtraRowCount());
+        height = (int)Math.floor(height * heightFactor);
+
         int columnCount = getColumnCount();
         int columnWidth = (int)Math.floor((double)width / (double)columnCount);
-        
+
         int rowCount = getRowCount();
         int rowHeight = (int)Math.floor((double)height / (double)rowCount);
-        
+
         int widthRemainder = width - (columnCount * columnWidth);
         int heightRemainder = height - (rowCount * rowHeight);
-        
+
         for (int i = 0; i < getChildCount(); i++) {
             View child = getChildAt(i);
-            
+
             if (child.getVisibility() == GONE) {
                 continue;
             }
-            
+
             LayoutParams params = (LayoutParams)child.getLayoutParams();
-            
+
             int childLeft = paddingLeft + (params.column * columnWidth) + params.leftAdjustment;
             int childTop = paddingTop + (params.row * rowHeight) + params.topAdjustment;
             int childWidth = params.columnSpan * columnWidth + params.widthAdjustment;
             int childHeight = params.rowSpan * rowHeight + params.heightAdjustment;
-            
+
             if (params.column + params.columnSpan == columnCount) {
                 // add the remaining space to the right most component
                 childWidth += widthRemainder;
@@ -279,32 +303,32 @@ public class GridLayout extends ViewGroup {
                 // add line between components
                 childWidth -= getColumnPadding();
             }
-            
+
             if (params.row + params.rowSpan == rowCount) {
                 // add the remaining space to the bottom most component
                 childHeight += heightRemainder;
             } else {
-                // add line between components                
+                // add line between components
                 childHeight -= getRowPadding();
             }
 
             child.measure(
-                MeasureSpec.makeMeasureSpec(childWidth, MeasureSpec.EXACTLY),
+                MeasureSpec.makeMeasureSpec(childWidth, MeasureSpec.EXACTLY), 
                 MeasureSpec.makeMeasureSpec(childHeight, MeasureSpec.EXACTLY)
             );
-            
+
             params.left = childLeft;
             params.top = childTop;
             params.right = childLeft + childWidth;
             params.bottom = childTop + childHeight;
         }
-        
+
         setMeasuredDimension(
             MeasureSpec.makeMeasureSpec(width, MeasureSpec.EXACTLY), 
             MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY)
         );
     }
-    
+
     /**
      * Layout the components.
      */
