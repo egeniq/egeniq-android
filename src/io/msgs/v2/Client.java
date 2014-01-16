5d5318737d7e8d8aec9e3005ee043acee3398f96
Özcan Kaymak <ozcan@egeniq.com>
2014-01-16 12:00:36 +0100

    Notification v2 implement (un)registerDevice.

--- src/io/msgs/v2/Client.java
+++ src/io/msgs/v2/Client.java
@@ -4,19 +4,25 @@ import io.msgs.v2.helper.EndpointRequestHelper;
 import io.msgs.v2.helper.UserRequestHelper;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.TimeZone;
 
 import org.json.JSONObject;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.util.Log;
 import ch.boye.httpclientandroidlib.HttpEntity;
+import ch.boye.httpclientandroidlib.NameValuePair;
 import ch.boye.httpclientandroidlib.client.entity.UrlEncodedFormEntity;
+import ch.boye.httpclientandroidlib.message.BasicNameValuePair;
 
 import com.egeniq.BuildConfig;
 import com.egeniq.utils.api.APIClient;
 import com.egeniq.utils.api.APIException;
+import com.egeniq.utils.api.APIUtils;
 
 /**
  * Msgs client.
@@ -28,14 +34,19 @@ public class Client {
     private final static String TAG = Client.class.getSimpleName();
     private final static boolean DEBUG = BuildConfig.DEBUG;
 
-    // private final static String NOTIFICATION_TAG = "NotificationManager";
-    // private final static String NOTIFICATION_TOKEN_KEY = "notificationToken";
+    private final static String DEVICE_TOKEN_KEY = "deviceToken";
+    private final static String USER_TOKEN_KEY = "userToken";
+    private final static String NOTIFICATION_TAG = "NotificationManager";
+    private final static String NOTIFICATION_TOKEN_KEY = "notificationToken";
 
     private final Context _context;
     private final String _serviceBaseURL;
     private final String _apiKey;
 
     private APIClient _apiClient;
+
+    private String _deviceToken;
+    private String _userToken;
 
     public final static SimpleDateFormat DATE_FORMAT;
     static {
@@ -69,15 +80,8 @@ public class Client {
         return _apiClient;
     }
 
-    //
-    // /**
-    // * Returns the current notification token.
-    // *
-    // * @return Notification token.
-    // */
-    // public String getNotificationToken() {
-    // return _context.getSharedPreferences(NOTIFICATION_TAG, Context.MODE_PRIVATE).getString(_apiKey + "." + NOTIFICATION_TOKEN_KEY, null);
-    // }
+    
+
 
     /**
      * Returns the Api Key.
@@ -93,12 +97,42 @@ public class Client {
      * @throws APIException
      */
     public void registerDevice(String deviceId) throws APIException {
-        // ingelogd? POST /users/:userToken/endpoints ==> ingelogd == userToken != null ??
-        // niet ingelogd? POST /endpoints
-
         try {
-            HttpEntity entity = new UrlEncodedFormEntity(null);
-            JSONObject object = _getAPIClient().post("/users", entity, false);
+            if (_getEndpointToken() != null && deviceId.equals(_getDeviceToken())) {
+                return;
+            }
+
+            if (_getUserToken() != null) {
+                List<NameValuePair> params = new ArrayList<NameValuePair>();
+                params.add(new BasicNameValuePair("userToken", _getUserToken()));
+                params.add(new BasicNameValuePair("type", "android-tablet")); // TODO:
+                params.add(new BasicNameValuePair("address", _getNotificationToken()));
+                params.add(new BasicNameValuePair("name", "My tablet")); //TODO:
+                params.add(new BasicNameValuePair("endpointSubscriptionsActive", "1")); //TODO:
+                params.add(new BasicNameValuePair("userSubscriptionsActive", "1")); //TODO:
+                
+                HttpEntity entity = new UrlEncodedFormEntity(params);
+                JSONObject object = _getAPIClient().post("/users/" + _getUserToken() + "/endpoints", entity, false);
+                
+                // Save token
+                SharedPreferences.Editor editor = _context.getSharedPreferences(NOTIFICATION_TAG, Context.MODE_PRIVATE).edit();
+                editor.putString(_apiKey + "." + DEVICE_TOKEN_KEY, APIUtils.getString(object, "token", null));
+                editor.commit();
+            } else {
+                List<NameValuePair> params = new ArrayList<NameValuePair>();
+                params.add(new BasicNameValuePair("type", "android-tablet")); // TODO:
+                params.add(new BasicNameValuePair("address", _getNotificationToken()));
+                params.add(new BasicNameValuePair("name", "My tablet")); //TODO:
+                params.add(new BasicNameValuePair("endpointSubscriptionsActive", "1")); //TODO:
+                params.add(new BasicNameValuePair("userSubscriptionsActive", "1")); //TODO:
+                HttpEntity entity = new UrlEncodedFormEntity(params);
+                JSONObject object = _getAPIClient().post("/endpoints", entity, false);
+                
+                // Save token
+                SharedPreferences.Editor editor = _context.getSharedPreferences(NOTIFICATION_TAG, Context.MODE_PRIVATE).edit();
+                editor.putString(_apiKey + "." + DEVICE_TOKEN_KEY, APIUtils.getString(object, "token", null));
+                editor.commit();
+            }
         } catch (Exception e) {
             if (DEBUG) {
                 Log.e(TAG, "Error registering device", e);
@@ -116,7 +150,9 @@ public class Client {
      * Unregister device.
      */
     public void unregisterDevice() {
-        // remove deviceToken from shared pref
+        SharedPreferences.Editor editor = _context.getSharedPreferences(NOTIFICATION_TAG, Context.MODE_PRIVATE).edit();
+        editor.remove(_apiKey + "." + DEVICE_TOKEN_KEY);
+        editor.commit();
     }
 
     /**
@@ -129,6 +165,7 @@ public class Client {
         // POST /users
         // indien device al bekend is (gebruiker was eerst niet ingelogd)
         // ook POST /users/:userToken/endpoints
+
         try {
             HttpEntity entity = new UrlEncodedFormEntity(null);
             JSONObject object = _getAPIClient().post("/users", entity, false);
@@ -176,10 +213,28 @@ public class Client {
     }
 
     /**
+    * Returns the current notification token.
+    *
+    * @return Notification token.
+    */
+    private String _getNotificationToken() {
+        return _context.getSharedPreferences(NOTIFICATION_TAG, Context.MODE_PRIVATE).getString(_apiKey + "." + NOTIFICATION_TOKEN_KEY, null);
+    }
+    
+    /**
+     * Returns the currently known device token.
+     * 
+     * @return Device token.
+     */
+    private String _getDeviceToken() {
+        return _context.getSharedPreferences(NOTIFICATION_TAG, Context.MODE_PRIVATE).getString(_deviceToken + "." + DEVICE_TOKEN_KEY, null);
+    }
+
+    /**
      * Get UserToken.
      */
     private String _getUserToken() {
-        return "user"; // TODO
+        return _context.getSharedPreferences(NOTIFICATION_TAG, Context.MODE_PRIVATE).getString(_userToken + "." + USER_TOKEN_KEY, null);
     }
 
     /**
@@ -188,4 +243,5 @@ public class Client {
     private String _getEndpointToken() {
         return "endpoint"; // TODO
     }
+
 }
