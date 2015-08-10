d1b86fc6c6a4047544863873aa0ff2067035114c
Jos van Egmond <josvanegmond@Joss-MacBook-Pro.local>
2015-08-10 13:35:42 +0200

    Supporting inline images. Made some methods for easier customization when extending. Added set/clear method.

--- src/com/egeniq/widget/SpannableTextView.java
+++ src/com/egeniq/widget/SpannableTextView.java
@@ -1,15 +1,18 @@
-package com.egeniq.widget;
+package nl.rtl.rtlz.widget;
 
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Typeface;
 import android.text.SpannableString;
 import android.text.Spanned;
+import android.text.style.DynamicDrawableSpan;
 import android.text.style.MetricAffectingSpan;
 import android.text.style.TextAppearanceSpan;
 import android.util.AttributeSet;
 
 import com.egeniq.utils.loader.FontLoader;
+import com.egeniq.widget.CustomTypefaceSpan;
+import com.egeniq.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,23 +25,60 @@ public class SpannableTextView extends TextView {
 
     private List<MarkupData> _markupList;
     private String _markupText;
+
     public SpannableTextView(Context context) {
         super(context);
-        _initialize();
+        clear();
     }
 
     public SpannableTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        _initialize();
+        clear();
     }
 
-    private void _initialize() {
+    /**
+     * Clears the content of the SpannableTextView.
+     */
+    public void clear() {
         _markupList = new ArrayList<MarkupData>();
         _markupText = "";
     }
 
     /**
-     * Appends a styled text to the TextView. Update() must be called after the final call.
+     * Overwrites the current content of this SpannableTextView with new content. Update() must be called to update the TextView visually.
+     *
+     * @param text Text to set
+     */
+    public void set(String text) {
+        clear();
+        append(text);
+    }
+
+    /**
+     * Appends a text to the SpannableTextView with default style. Update() must be called to update the TextView visually.
+     * This method also obtains any custom font applied to the style via the font attribute.
+     *
+     * @param text Text to append
+     */
+    public void append(String text) {
+        TextAppearanceSpan textAppearanceSpan = new TextAppearanceSpan(getContext(), android.R.style.TextAppearance);
+        _markupList.add(new MarkupData(text, textAppearanceSpan));
+        _markupText += text;
+    }
+
+    /**
+     * Overwrites the current content of this SpannableTextView with new content. Update() must be called to update the TextView visually.
+     *
+     * @param style R.style resource to style the text with
+     * @param text  Text to set
+     */
+    public void set(int style, String text) {
+        clear();
+        append(style, text);
+    }
+
+    /**
+     * Appends a styled text to the SpannableTextView. Update() must be called to update the TextView visually.
      * This method also obtains any custom font applied to the style via the font attribute.
      *
      * @param style R.style resource to style the text with
@@ -64,8 +104,47 @@ public class SpannableTextView extends TextView {
     }
 
     /**
-     * Updates the TextView with markup text from the _markupText collection.
-     * Each text segment is appended to the last.
+     * Overwrites the current content of this SpannableTextView with new content. Update() must be called to update the TextView visually.
+     *
+     * @param span the MetricAffectingSpan to be applied
+     */
+    public void set(DynamicDrawableSpan span) {
+        clear();
+        append(span);
+    }
+
+    /**
+     * Appends a MetricAffectingSpan to the SpannableTextView. Update() must be called to update the TextView visually.
+     *
+     * @param span
+     */
+    public void append(DynamicDrawableSpan span) {
+        _markupList.add(new MarkupData(" ", span));
+        _markupText += " ";
+    }
+
+    /**
+     * Overwrites the current content of this SpannableTextView with new content. Update() must be called to update the TextView visually.
+     *
+     * @param span the MetricAffectingSpan to be applied
+     */
+    public void set(MetricAffectingSpan span) {
+        clear();
+        append(span);
+    }
+
+    /**
+     * Appends a MetricAffectingSpan to the SpannableTextView. Update() must be called to update the TextView visually.
+     *
+     * @param span
+     */
+    public void append(MetricAffectingSpan span) {
+        _markupList.add(new MarkupData(" ", span));
+        _markupText += " ";
+    }
+
+    /**
+     * Updates the TextView with the appended Spans.
      */
     public void update() {
         SpannableString text = new SpannableString(this._markupText);
